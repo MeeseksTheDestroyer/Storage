e18ad129c31fb970ef19224465b9565c5815c68f
MeeseksTheDestroyer <117534219+MeeseksTheDestroyer@users.noreply.github.com>
2024-02-26 10:18:16 +0100

    Cos dziala

--- src/libs/zipper/index.js
+++ src/libs/zipper/index.js
@@ -1,24 +1,57 @@
 const fs = require('fs');
 const zlib = require('zlib');
+const path = require('path');
 
-function zipAndLog(filename){
-    const readStream = fs.createReadStream(filename)
-    const gzip = zlib.createGzip()
-
-    readStream.pipe(gzip)
-
-    gzip.on('data', (chunk) => {
-        console.log(chunk)
-    })
-
-    gzip.on('error', (err) => {
-        return err.toString();
-    })
-
-    gzip.on('end', (chunk) => {
-        return 'Complete';
-    })
+function bufferToBits(buffer) {
+    let bits = '';
+    for (const byte of buffer) {
+        console.log(byte)
+    }
 }
 
-const filename = "C:\\Users\\PC\\Desktop\\Pro\\Storage\\src\\tests\\test.txt"
-zipAndLog(filename)
+function zipAndLog(filename, callback){
+    const filePath = path.join(__dirname, filename);
+
+    const readStream = fs.createReadStream(filePath);
+    const gzip = zlib.createGzip();
+    var tab_zip = "";
+
+    readStream.pipe(gzip);
+
+    gzip.on('data', (chunk) => {
+        bufferToBits(chunk)
+    });
+
+    gzip.on('error', (err) => {
+        console.error(err.toString());
+    });
+
+    gzip.on('end', () => {
+        console.log('Complete');
+        callback(null, tab_zip)
+    });
+}
+
+function unzip(tab_zipper){
+    const binaryData = Buffer.from(tab_zipper, 'binary');
+
+    // Dekompresja danych za pomocą zlib
+    zlib.gunzip(binaryData, (err, decompressedData) => {
+        if (err) {
+            console.error('Decompression Error:', err);
+            return;
+        }
+        // Wyświetlenie zdekompresowanych danych jako tekst
+        console.log('Decompressed Data:', decompressedData.toString());
+    });
+}
+
+const filename = "test.txt";
+const zip = zipAndLog(filename, async (err, zip) => {
+    if (err) {
+        console.error('Error:', err);
+        return;
+    }
+    
+    console.log(unzip(zip))
+});
